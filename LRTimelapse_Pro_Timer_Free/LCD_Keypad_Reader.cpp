f46c132272a624b3aa9e054f5b25204bb7b4f3d2
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2017-07-06 20:37:22 +0200

    Updated LCD_Keypad_Reader.cpp with values from my shield

--- LRTimelapse_Pro_Timer_Free/LCD_Keypad_Reader.cpp
+++ LRTimelapse_Pro_Timer_Free/LCD_Keypad_Reader.cpp
@@ -7,12 +7,15 @@ static int DEFAULT_THRESHOLD = 5;
 // The Sainsmart keypad uses a voltage divider to deliver a voltage  
 // between 0 and 5 V that corresponds to the key being pressed in  
 // order to use only a single input pin. The values below are from 0 to  
-// 1023 because the Arduino uses a 10 bit resolution. 
-static int UPKEY_ARV = 144; // 0.720 V, that's read "analogue read value" 
-static int DOWNKEY_ARV = 329; // 1.645 V 
-static int LEFTKEY_ARV = 505; // 2.525 V 
+// 1023 because the Arduino uses a 10 bit resolution.
+// I've used the values from my LCD Keypad Shield. To check out your values check
+// https://gist.github.com/rudelm/463c59a218a4c17fdad8228356446f8b
+
+static int UPKEY_ARV = 131; // 0.720 V, that's read "analogue read value" 
+static int DOWNKEY_ARV = 308; // 1.645 V 
+static int LEFTKEY_ARV = 481; // 2.525 V 
 static int RIGHTKEY_ARV = 0; // 0 V 
-static int SELKEY_ARV = 742; // 3.710 V 
+static int SELKEY_ARV = 723; // 3.710 V 
 static int NOKEY_ARV = 1023; // 5.115 V 
 
 LCD_Keypad_Reader::LCD_Keypad_Reader() 
